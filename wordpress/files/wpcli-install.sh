cc9ddccea6a0b3089116cf80b86db822e6d7b196
Del_S <david@sucharda.cz>
2015-03-14 19:17:16 +0100

    Auto complete for WP CLI

--- wordpress/files/wpcli-install.sh
+++ wordpress/files/wpcli-install.sh
@@ -6,3 +6,26 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 #Installing wp-cli
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
+
+# bash completion for the `wp` command
+sudo -i
+
+_wp_complete() {
+	local cur=${COMP_WORDS[COMP_CWORD]}
+
+	IFS=$'\n';  # want to preserve spaces at the end
+	local opts="$(wp cli completions --line="$COMP_LINE" --point="$COMP_POINT" --allow-root)"
+
+	if [[ "$opts" =~ \<file\>\s* ]]
+	then
+		COMPREPLY=( $(compgen -f -- $cur) )
+	elif [[ $opts = "" ]]
+	then
+		COMPREPLY=( $(compgen -f -- $cur) )
+	else
+		COMPREPLY=( ${opts[*]} )
+	fi
+}
+complete -o nospace -F _wp_complete wp --allow-root
+
+sudo -u ubuntu -i
